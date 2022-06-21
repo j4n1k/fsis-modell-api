893a292ab4a08b7de56262713bb4a8f4e0d4c615
Janik Bischoff <janikbischoff@MacBook-Pro-von-Janik.local>
2022-06-21 21:22:47 +0200

    heroku req

--- api.py
+++ api.py
@@ -3,13 +3,23 @@ import pickle
 
 app = Flask(__name__)
 
-@app.route("/predict/<modelparams>")
-def predict(modelparams):
+
+@app.route("/")
+def index():
+    return "<h1>Usage: url/predict/<password>/<modelparams></h1>"
+
+@app.route("/predict/<password>/<modelparams>")
+def predict(password, modelparams):
     #TODO Import Model
-    if modelparams:
-        result = modelparams
-        #result = model.predict(modelparams)
+    if password == "FSIS2022":
+        if modelparams:
+            result = modelparams
+            #result = model.predict(modelparams)
+        else:
+            result = {"ETA":"10 Tage"}
+        return jsonify(result)
     else:
-        result = {"ETA":"10 Tage"}
-    return jsonify(result)
+        result = "Falscher Schlüssel"
+        return jsonify(result)
+
 
